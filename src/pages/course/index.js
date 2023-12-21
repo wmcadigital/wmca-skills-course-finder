19096df422675aac99d88d2ea50a3b8fc27c9804
lsnomensa <lscott@nomensa.com>
2023-12-21 16:37:54 +0000

    number change

--- src/pages/course/index.js
+++ src/pages/course/index.js
@@ -185,7 +185,7 @@ const Page = () => {
           <h2>Get help and advice</h2>
           <p>Not sure which course is right for you? Our partners at National Careers Service are on hand to help</p>
           <p className="mtb-10"><strong>Live chat:</strong> <a className="wmcads-link" href="https://nationalcareers.service.gov.uk/webchat/chat" target="_blank" rel="noopener noreferrer">Speak to an adviser on webchat</a></p>
-          <p className="mtb-10"><strong>Phone:</strong> <a className="wmcads-link" href={`tel:01213456789`}>0121 345 6789</a></p>
+          <p className="mtb-10"><strong>Phone:</strong> <a className="wmcads-link" href={`tel:0800100900`}>0800 100 900</a></p>
         </div>
       </div>
     )
